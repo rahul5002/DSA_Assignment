a7ddfe5b242115b70f2b429405ad07826666af1d
rahul5002 <vishalranka763@gmail.com>
2026-08-21 00:43:01 +0530

    Day21 commit

---
+++ Day21_TraverseSinglyLinkedList.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node* next;
+};
+int main() 
+{
+    int n;
+    if (scanf("%d", &n) != 1) 
+    {
+        return 1;
+    }
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    for (int i = 0; i < n; i++) 
+    {
+        int val;
+        scanf("%d", &val);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+        if (head == NULL) 
+        {
+            head = newNode;
+            tail = newNode;
+        } 
+        else 
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    struct Node* current = head;
+    while (current != NULL) 
+    {
+        printf("%d", current->data);
+        if (current->next != NULL) 
+        {
+            printf(" ");
+        }
+        current = current->next;
+    }
+    printf("\n");
+    current = head;
+    while (current != NULL) 
+    {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    
+    return 0;
+}
