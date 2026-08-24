f002e0ae7d7a3ac10b50ea69222417aa36046406
rahul5002 <vishalranka763@gmail.com>
2026-08-24 22:43:50 +0530

    Day25 commit

---
+++ Day25_countOccurences.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() 
+{
+    int n;
+    if (scanf("%d", &n) != 1) 
+    {
+        return 0;
+    }
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    for (int i = 0; i < n; i++) 
+    {
+        int val;
+        scanf("%d", &val);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+        if (head == NULL) 
+        {
+            head = newNode;
+            tail = newNode;
+        } 
+        else 
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    int key;
+    if (scanf("%d", &key) != 1) 
+    {
+        return 0;
+    }
+    int count = 0;
+    struct Node* current = head;
+    while (current != NULL) 
+    {
+        if (current->data == key) 
+        {
+            count++;
+        }
+        current = current->next;
+    }
+    printf("%d\n", count);
+    current = head;
+    while (current != NULL) 
+    {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
+}
